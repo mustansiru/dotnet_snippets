ba2c2ab11b01b3774d01b3115baa2ab779d4a62d
Mustansir Udaipurwala <mhusain@mhusa.in>
2019-10-10 11:45:39 +0530

    Observer pattern sample snippet

--- design_patterns/Observer/Pattern/Investor.cs
+++ design_patterns/Observer/Pattern/Investor.cs
@@ -16,6 +16,8 @@ namespace Observer.Pattern
 
         public void Update(Stock stock)
         {
+            // Here process based on the updates 
+            // received for the observed object.
             Console.WriteLine(@"Notified {0} of {1}'s change to {2:C}",
                 this.Name, stock.Symbol, stock.Price);
         }
